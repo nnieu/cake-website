593f653817bdf162ee2bf6c58241f7666a9818f0
nnieu <annie.2005@outlook.com>
2025-05-31 00:48:17 -0500

    W O W !

--- backend.js
+++ backend.js
@@ -1,1 +1,23 @@
+// index.js
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+// You can change the port or rely on an environment variable
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.static(path.join(__dirname, 'templates')));
+// Define a simple GET route at “/”
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'templates', 'stolen.html'));
+});
+
+// Start listening on the specified port
+app.listen(PORT, () => {
+  console.log(`🚀 Server started on http://localhost:${PORT}`);
+});
+
+
 
